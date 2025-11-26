8797d8386d8bc8ab396edbeaf13aed2d56ef62e8
blarrain <blarrain2@uc.cl>
2025-11-26 19:21:37 -0300

    middleware basico

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import joyasRoutes from './routes/joyas.route.js';
+import { joyasLog } from './middleware/joyas.middleware.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +10,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(joyasLog)
+
 app.use('/joyas', joyasRoutes);
+
+
+//RUTA por defecto - error 404
+/* app.get('*', (req, res) => {
+	res.status(404).send('Esta ruta no existe');
+}); */
 
 app.listen(PORT, console.log(`🟢 Servidor arriba: http://localhost:${PORT}`));
